74ee2eba3bce35140bbcac9eb0ba63851ca8062b
Zachary DeParle <zachary.deparle@duke.edu>
2023-12-29 17:29:49 -0500

    Initial Commit

---
+++ MEMSCalc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MEMSCalc
+//
+//  Created by Zachary DeParle on 12/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MEMSCalc/MEMSCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  MEMSCalcApp.swift
+//  MEMSCalc
+//
+//  Created by Zachary DeParle on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct MEMSCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
